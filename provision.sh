2734d9027a4e0f92b75b55882b7f8a2d5881aab3
artemave <artemave@gmail.com>
2015-08-01 23:31:48 +0100

    expose sinatra port

--- provision.sh
+++ provision.sh
@@ -17,9 +17,11 @@ if [[ ! $mongo_is_running ]]; then
 fi
 
 docker build -t oompa .
+docker build -t oompa-auth -f Dockerfile-auth .
 
-docker build -t oompa-auth -f Dockerfile-auth
-docker run -d --link mongodb:mongodb -e TWITTER_KEY=$TWITTER_KEY -e TWITTER_SECRET=$TWITTER_SECRET --name authorize-accounts oompa-auth
+docker stop authorize-accounts || :
+docker rm authorize-accounts || :
+docker run -p '4567:4567' -d --link mongodb:mongodb -e TWITTER_KEY=$TWITTER_KEY -e TWITTER_SECRET=$TWITTER_SECRET --name authorize-accounts oompa-auth
 
 docker stop tweet_sender || :
 docker rm tweet_sender || :
