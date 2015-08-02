42a6f176d2c20942c547a11f365c85c9cb693db6
artemave <artemave@gmail.com>
2015-08-02 14:57:52 +0100

    reflect new fields in the model structure

--- lib/models/account.rb
+++ lib/models/account.rb
@@ -5,7 +5,8 @@ class Account
   include Mongoid::Document
 
   field :username
-  field :password
+  field :access_token
+  field :access_secret
   embeds_many :tweets
 
   def minimum_acceptable_score
